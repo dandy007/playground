209ed6494cd3e2602191d4996a9ad96e6ca188b3
Jan Danisik <jandanisik@gmail.com>
2018-11-10 17:21:52 +0100

    Parameterized test - usecase names

--- test-junit4/src/test/java/com/jdanisik/ParameterizedTest.java
+++ test-junit4/src/test/java/com/jdanisik/ParameterizedTest.java
@@ -13,28 +13,30 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class ParameterizedTest {
 
-	@Parameters
-	public static Collection<Object[]> data() {
-		return Arrays.asList(new Object[][] {
-			{ 3, 9 }, 
-			{ 5, 25 }
-		});
-	}
+//  @Parameters(name="{index}: sort[{0}]={1}")
+  @Parameters(name="{0}")
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][] {
+      { "Usecase 1", 5, 25 },
+      { "Usecase 2", 3, 9 },
+    });
+  }
 
-	private final int input;
-	private final int expectedResult;
+  private final int input;
+  private final int expectedResult;
 
-	public ParameterizedTest(
-			int input, 
-			int expectedResult
-			) {
-		this.input = input;
-		this.expectedResult = expectedResult;
-	}
+  public ParameterizedTest(
+      String name,
+      int input,
+      int expectedResult
+      ) {
+    this.input = input;
+    this.expectedResult = expectedResult;
+  }
 
-	@Test
-	public void testPow() {
-		assertEquals(expectedResult, (int)Math.pow(input, 2));
-	}
+  @Test
+  public void testPow() {
+    assertEquals(expectedResult, (int)Math.pow(input, 2));
+  }
 
 }
