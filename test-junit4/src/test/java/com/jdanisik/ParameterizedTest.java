ef540af88ecc48530fbcbc004dde1257a284c9bd
Jan Danisik <jandanisik@gmail.com>
2018-11-10 17:33:34 +0100

    Test annotations

--- test-junit4/src/test/java/com/jdanisik/ParameterizedTest.java
+++ test-junit4/src/test/java/com/jdanisik/ParameterizedTest.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -12,6 +16,26 @@ import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
 public class ParameterizedTest {
+
+  @BeforeClass
+  public static void beforeClass() {
+    System.out.println("beforeClass");
+  }
+
+  @Before
+  public void before() {
+    System.out.println("before");
+  }
+
+  @After
+  public void after() {
+    System.out.println("after");
+  }
+
+  @AfterClass
+  public static void afterClass() {
+    System.out.println("afterClass");
+  }
 
 //  @Parameters(name="{index}: sort[{0}]={1}")
   @Parameters(name="{0}")
@@ -22,6 +46,7 @@ public class ParameterizedTest {
     });
   }
 
+  private final String name;
   private final int input;
   private final int expectedResult;
 
@@ -30,12 +55,14 @@ public class ParameterizedTest {
       int input,
       int expectedResult
       ) {
+    this.name = name;
     this.input = input;
     this.expectedResult = expectedResult;
   }
 
   @Test
   public void testPow() {
+    System.out.println(name);
     assertEquals(expectedResult, (int)Math.pow(input, 2));
   }
 
