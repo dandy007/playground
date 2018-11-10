431deb3b7bff378f5727bec4f357cb45e1b76115
Jan Danisik <jandanisik@gmail.com>
2018-11-10 15:23:31 +0100

    Suit test example

---
+++ test-junit4/src/test/java/com/jdanisik/SuiteExample.java
@@ -0,0 +1,14 @@
+package com.jdanisik;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+  ParameterizedTest.class
+})
+
+public class SuiteExample {
+
+}
