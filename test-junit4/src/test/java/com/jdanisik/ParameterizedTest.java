bc702804a80cd37731042bb33a7e952373193bbf
Jan Danisik <jandanisik@gmail.com>
2018-11-10 17:45:55 +0100

    refactoring + rule usage

--- test-junit4/src/test/java/com/jdanisik/ParameterizedTest.java
+++ test-junit4/src/test/java/com/jdanisik/ParameterizedTest.java
@@ -5,10 +5,6 @@ import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
 import java.util.Collection;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -16,26 +12,6 @@ import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
 public class ParameterizedTest {
-
-  @BeforeClass
-  public static void beforeClass() {
-    System.out.println("beforeClass");
-  }
-
-  @Before
-  public void before() {
-    System.out.println("before");
-  }
-
-  @After
-  public void after() {
-    System.out.println("after");
-  }
-
-  @AfterClass
-  public static void afterClass() {
-    System.out.println("afterClass");
-  }
 
 //  @Parameters(name="{index}: sort[{0}]={1}")
   @Parameters(name="{0}")
--- test-junit4/src/test/java/com/jdanisik/Tests.java
+++ test-junit4/src/test/java/com/jdanisik/Tests.java
@@ -1,11 +1,42 @@
 package com.jdanisik;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 
 public class Tests {
 
+  @Rule
+  public TestName name = new TestName();
+
+  @BeforeClass
+  public static void beforeClass() {
+    System.out.println("beforeClass");
+  }
+
+  @Before
+  public void before() {
+    System.out.println("before");
+  }
+
+  @After
+  public void after() {
+    System.out.println("after");
+  }
+
+  @AfterClass
+  public static void afterClass() {
+    System.out.println("afterClass");
+  }
+
   @Test(expected=NumberFormatException.class)
   public void testExpected() {
+    System.out.println(name.getMethodName());
+
     Long.valueOf("a123"); // throw number format exception
   }
 
